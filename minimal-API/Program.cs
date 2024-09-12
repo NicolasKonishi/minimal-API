477848edd7e9d1f729b881767a165742b148f6db
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-09-12 20:47:10 -0300

    Adicionar arquivos de projeto.

---
+++ minimal-API/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello world!");
+
+app.MapPost("/login", (LoginDTO loginDto) =>
+{
+    if(loginDto.Email == "adm@gmail.com" && loginDto.Senha == "1234")
+    {
+        return Results.Ok("login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+
+app.Run();
+
+public class LoginDTO()
+{
+    public string Email { get; set; } = default;
+    public string Senha { get; set; } = default;
+}
