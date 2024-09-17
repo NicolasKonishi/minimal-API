0e1b14deb65e869aa0c413237e01cbd57f0311c6
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2024-09-16 22:53:33 -0300

    meoto post

---
+++ minimal-API/Dominio/DTO/VeiculoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace minimal_API.Dominio.DTO
+{
+    public record VeiculoDTO
+    {
+        public string Nome { get; set; } = default!;
+        public string Marca { get; set; } = default!;
+        public int Ano { get; set; } = default!;
+    }
+}
--- minimal-API/Program.cs
+++ minimal-API/Program.cs
@@ -1,15 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using minimal_API.Dominio.DTO;
+using minimal_API.Dominio.Entidades;
 using minimal_API.Dominio.Interfaces;
 using minimal_API.Dominio.ModelViews;
 using minimal_API.Dominio.Servicos;
 using minimal_API.Infraestrutura.DB;
 
-
+#region builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServicos>();
+builder.Services.AddScoped<IVeiculosServico, VeiculoServico>();
 
 
 builder.Services.AddControllers();
@@ -22,17 +25,18 @@ builder.Services.AddDbContext<DBContexto>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("conexao"));
 });
 
-
 var app = builder.Build();
+#endregion
 
-
-app.UseSwagger();
-app.UseSwaggerUI();
+#region Home
 
 app.MapGet("/", () => Results.Json(new Home()));
 
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+#region adms
+
+app.MapPost("/adms/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
     if(administradorServico.Login(loginDTO) != null )
     {
@@ -43,6 +47,34 @@ app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico admin
         return Results.Unauthorized();
     }
 });
+#endregion
 
 
+#region veiculos
+
+app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculosServico veiculoServico) =>
+{
+
+    var veiculo = new Veiculo
+    {
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano
+
+    };
+
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculo/{veiculo.Id}",veiculo);
+
+
+});
+
+#endregion
+
+#region app
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Run();
+
+#endregion
